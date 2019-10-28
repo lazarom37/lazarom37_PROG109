b243db812a50c72dea307c7f16cebfc9163f308c
Marcus Lazaro PROG109 <lazaromarcuslazaro@gmail.com>
2019-10-28 12:19:35 -0700

    You ever wonder why we dont use the word Java for coffee these days

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,25 @@
+const myString = "Learning Javascript in class.";
+const myFirstNumericVariable = 3;
+const myBoolean = true;
+const myNull = null;
+let myUndefined;
+
+var oldStyle = "Oldies way of doin' it";
+const constantVariable = "This variable will not change";
+let newChangeableStyle = "This lets you declare a variable that can change";
+
+const quotable = "She sells seashells on eBay"
+
+console.log(myString);
+console.log('TYPE', typeof myString);
+console.log(myFirstNumericVariable);
+console.log('TYPE', myFirstNumericVariable);
+console.log(myBoolean);
+console.log('TYPE', myBoolean);
+console.log(myNull);
+console.log('TYPE', myNull);
+console.log(myUndefined);
+console.log('TYPE', myUndefined);
+console.log('ADD 3 + 25 =', myFirstNumericVariable + 25);
+
+console.log('ADD UNEDFINED =', myUndefined + 25);
